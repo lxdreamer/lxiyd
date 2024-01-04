8ec8ebe690c172ebbb60f0e4054fa0de2d292329
lixiangbj04 <lixiangbj04@kanyun.com>
2024-01-04 21:07:04 +0800

    MOD: 修改主页

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,34 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.wrapper}>
+      <div className={styles["newsletter"]}>
+      <div className={styles["newsletter-main"]}>
+        <div className={styles["stamp"]}>
+          <img src="/letter/stamp.png" alt="" />
+        </div>
+        <h2>致亲爱的yudi</h2>
+        <div className={styles["yayun"]}>本信件非常重要，由lixiang押运。</div>
+        <div className={styles["design"]}>
+          <img className={styles["designImg"]} src="/letter/design.png" alt="" />
+        </div>
+        <p>愿得一心人，白头不相离。</p>
+        <div className={styles["design"]}>
+          <img className={styles["designImg"]} src="/letter/design.png" alt="" />
+        </div>
+        <div className={styles["tlg"]}>
+          <div className={styles["tlg-img"]}>
+            <img src="/letter/tlg.png" alt="" />
+          </div>
+          <div className={styles["tlg-text"]}>
+            {/* <h3>lixiang</h3> */}
+            {/* <h4>我爱你</h4> */}
+          </div>
+          <div className={styles["clear"]}> </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
+  </div>
   )
 }
